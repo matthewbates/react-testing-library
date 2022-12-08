d5671fe98b5ae2496c49270bee674232c854ac6e
Matthew <matthewhcbates@gmail.com>
2022-12-08 13:21:09 -0700

    Test and code button click

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [color, setColor] = useState("red");
+
+  const newButtonColor = color === "red" ? "blue" : "red";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button
+        onClick={() => setColor(newButtonColor)}
+        style={{ background: color }}
+      >
+        Change to {newButtonColor}
+      </button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import { logRoles } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+// string description and test function
+// the first thing you want to do after the string description is defined is render the component
+// next, find the element you're interested in by screen that has access to the virtual DOM by render
+test("button has correct initial color", () => {
   render(<App />);
-  const linkElement = screen.getByText("Learn React");
-  expect(linkElement).toBeInTheDocument();
+  // first argument will be a string representing the role (button)
+  // second argument is the options (as a string or regex)
+  const colorButton = screen.getByRole("button", { name: /change to blue/i });
+  // expect the background color to be red
+  expect(colorButton).toHaveStyle({ background: "red" });
+  // click button
+  fireEvent.click(colorButton);
+  // expect the background color to be blue
+  expect(colorButton).toHaveStyle({ background: "blue" });
+  // expect the button text to be /change to red/i
+  expect(colorButton).toHaveTextContent(/change to red/i);
 });
